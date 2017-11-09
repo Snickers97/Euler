c2ec7bc6d64c77bfb6ae2d32d4b90b68d19d0a1a
Jacob Handley <jacobhandley50@gmail.com>
2017-11-09 10:31:37 -0500

    15.cc

--- code/15.cc
+++ code/15.cc
@@ -6,29 +6,31 @@ using namespace std;
 int main(){
 	int count = 0;
 	int paths = 0;
-	string unique[10000];
-	for(int i = 0; i < 10000; i++){
+	string unique[1000000];
+	for(int i = 0; i < 1000000; i++){
 		unique[i] = "";
 	}
 	while(count < 1000000){
 		int right  = 0;
 		int down = 0;
 		string attempt = "";
-		while((right + down) <= 40){
+		while((right + down) < 40){
 			int flip = rand()%10000;
-			if(flip > 5000){
+			if(flip > 5000 && right < 20){
 				right += 1;
 				attempt += 'r';
 			}
-			else{
+			else if(down < 20){
 				down += 1;
 				attempt += 'l';
 			}
 		}
 		bool isunique = true;
 		for(int i = 0; i < paths; i++){
-			if(unique[i] == attempt)
+			if(attempt == unique[i]){
 				isunique = false;
+				break;
+			}
 		}
 		if(isunique){
 			unique[paths] = attempt;
