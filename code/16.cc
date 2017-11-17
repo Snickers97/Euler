ca89ad95fd721a27baa76464e1a566270e5114b3
Jacob Handley <jacobhandley50@gmail.com>
2017-11-16 21:23:00 -0500

    Made progress on 16

--- code/16.cc
+++ code/16.cc
@@ -6,10 +6,20 @@ using namespace std;
 int charnum(int &i);
 
 int main(){
-
+	int amount;
+	cout<<"Enter the desired amount. \n";
+	cin>>amount;
+	int total = 0;
+	for(int i = 1; i <= amount; i++){
+		total += charnum(i);
+	}
+	cout<<total<<endl;
 	return 0;
 }
 
 int charnum(int &i){
-	if
+	switch(i){
+		case 1:
+
+	}
 }
